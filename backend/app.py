f524596ea89b7b5763ed49270ebb98cead0e3196
ericliu-12 <133845008+alaska-summer@users.noreply.github.com>
2024-11-10 01:57:02 -0600

    example stuff

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request, jsonify
 from model import generate_llm_response
 import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/generate', methods=['POST'])
 def system_design():
+    # try:
+    #     data = request.get_json()
+    #     user_input = data.get("userInput")
+
+    #     response = generate_llm_response(user_input)
+
+    #     if isinstance(response, str):
+    #         response_content = json.loads(response)
+
+    #     return jsonify(response_content)
     try:
-        data = request.get_json()
-        user_input = data.get("userInput")
-
-        response = generate_llm_response(user_input)
-
-        if isinstance(response, str):
-            response_content = json.loads(response)
-
-        return jsonify(response_content)
+        json_path = os.path.join('public', 'example.json')
+        with open(json_path, 'r') as json_file:
+            data = json.load(json_file)
+        return jsonify(data)
+    
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
